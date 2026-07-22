8e592e44fc1380dc8129b2d3562b36bdb3c7c110
ANANYA <ananyasinha788@gmail.com>
2026-07-22 11:55:58 +0530

    num in figure

--- lab 1/hi.js
+++ lab 1/hi.js
@@ -29,4 +29,37 @@ const numberToWord = (num) => {
   }
 };
 
-console.log(numberToWord(5));
+
+
+
+// Function 1: Convert a single digit into words
+const numberToWord = (num) => {
+    switch (num) {
+        case 0: return "Zero";
+        case 1: return "One";
+        case 2: return "Two";
+        case 3: return "Three";
+        case 4: return "Four";
+        case 5: return "Five";
+        case 6: return "Six";
+        case 7: return "Seven";
+        case 8: return "Eight";
+        case 9: return "Nine";
+        default: return "Invalid";
+    }
+};
+
+// Function 2: Convert a complete number into words
+const showNumberInWords = (num) => {
+    let result = "";
+
+    let digits = String(num);
+
+    for (let digit of digits) {
+        result = result + numberToWord(Number(digit)) + " ";
+    }
+
+    return result;
+};
+
+console.log(showNumberInWords(12345));
