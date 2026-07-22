799901ffbdac80853da28fc391f083925ed94fdc
ANANYA <ananyasinha788@gmail.com>
2026-07-22 11:14:37 +0530

    number in words 2

--- lab 1/hi.js
+++ lab 1/hi.js
@@ -1,23 +1,32 @@
 /*write a function to take  number between 0 to 9 and return in words through arrow gunction*/
-const numberToWord = (num) => {
-    const words = [
-        "Zero",
-        "One",
-        "Two",
-        "Three",
-        "Four",
-        "Five",
-        "Six",
-        "Seven",
-        "Eight",
-        "Nine"
-    ];
 
-    if (num >= 0 && num <= 9) {
-        return words[num];
-    } else {
-        return "Invalid number";
-    }
+
+console.log(numberToWord(5));
+const numberToWord = (num) => {
+  switch (num) {
+    case 0:
+      return "Zero";
+    case 1:
+      return "One";
+    case 2:
+      return "Two";
+    case 3:
+      return "Three";
+    case 4:
+      return "Four";
+    case 5:
+      return "Five";
+    case 6:
+      return "Six";
+    case 7:
+      return "Seven";
+    case 8:
+      return "Eight";
+    case 9:
+      return "Nine";
+    default:
+      return "Invalid Number";
+  }
 };
 
-console.log(numberToWord(5))
+console.log(numberToWord(5));
